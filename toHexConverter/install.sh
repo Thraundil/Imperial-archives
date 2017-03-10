24ab0515bf7215584f11679665fe053661a49443
thraundil <emil.bak@hotmail.com>
2017-03-10 11:34:00 +0100

    updated toHexConverter

--- toHexConverter/install.sh
+++ toHexConverter/install.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 # Put this file (together with toHex.py and tohex) into your Downloads folder
 
-cd
+cd ~/
 mkdir bin
 mkdir bin/toHex
 cd ../Downloads
 mv toHex.py ../bin/
 mv tohex ../bin/
-cd
-cd bin
+cd ~/bin
 chmod u+x tohex
 cd
 export PATH="~/bin:$PATH"
