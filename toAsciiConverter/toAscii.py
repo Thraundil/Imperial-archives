51381520823c9e804121cbd31152ab2523969de6
thraundil <emil.bak@hotmail.com>
2017-04-13 20:33:46 +0200

    ascii converter

---
+++ toAsciiConverter/toAscii.py
@@ -0,0 +1,70 @@
+# Af Emil S. Bak @ DIKU, 2017
+
+import sys, getopt
+
+def converterDec(arg):
+    chars = []
+    output = []
+    twoInt = 0
+    threeInt = 0
+    i = 0
+
+    for c in arg:
+        chars.append(c)
+    while i < (len(chars)):
+        strengen = ''
+        if (chars[i] != '1') and (twoInt != 1) and (threeInt == 0):
+            strengen = chars[i]
+            strengen = strengen + chars[i+1]
+            output.append(chr(int(strengen)))
+            twoInt = 1
+            i += 1
+        elif (chars[i] == '1') and (twoInt != 1) and (threeInt == 0):
+            strengen = chars[i]
+            strengen = strengen + chars[i+1]
+            strengen = strengen + chars[i+2]
+            output.append(chr(int(strengen)))
+            i += 1
+            threeInt = 2
+        else:
+            i += 1
+            twoInt = 0
+            if (threeInt != 0):
+                threeInt = threeInt - 1
+
+    print (''.join(output))
+
+
+
+def converter(arg, base, k):
+    pair = [arg[i:i+k] for i in range(0, len(arg), k)]
+    for n in range(0, len(pair)):
+        try:
+            hexConverted = (hex(int(pair[n],base)))
+            hexConverted = hexConverted[2:]
+            sys.stdout.write (hexConverted.decode("hex"))
+
+        except ValueError:
+            print ("Wrong input compared to base")
+
+def main(argv):
+    try:
+        opts, args = getopt.getopt(argv,"b:d:o:h:",["binary","decimal","hex"])
+    except getopt.GetoptError:
+        print ("Wrong input - Try again (-b, -d, -h)")
+        print ("toAscii [-argument] [input]")
+        sys.exit(2)
+
+    for opt, arg in opts:
+        if opt in ("-b","--binary"):
+            converter(arg,2,8)
+            print ("")
+        if opt in ("-d","--decimal"):
+            converterDec(arg)
+        if opt in ("-h","--hex"):
+            converter(arg,16,2)
+            print ("")
+
+# Makes sure the MAIN is run
+if __name__ == '__main__':
+    main(sys.argv[1:])
