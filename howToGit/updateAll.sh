5e1dcdf4b7b4481bd02ddcca78e0bedf7d567a3d
thraundil <emilsbak@gmail.com>
2017-11-05 20:11:16 +0100

    updated updater

--- howToGit/updateAll.sh
+++ howToGit/updateAll.sh
@@ -1,19 +1,20 @@
 #!/usr/bin/env sh
 # By Emil S. Bak, 2017 DIKU
 
+standard="repo0 repo1 repo2"
+
+upstream="repoUp0 repoUp1 repoUp2"
 
 # All repositories in need of a standard 'Git Pull' goes here.
-for repo in cokepc infoscreen imperial-archives national-platform website bornhack-ctf ap-2017; do
-    (
-    echo "\033[1;31m*************** Now pulling ${repo} ***************\033[0m"
+for repo in $standard; do
+    (echo "\033[1;31m*************** Now pulling ${repo} ***************\033[0m"
     cd "${repo}" && git pull)
 done
 
 # Forked repositories goes here, remember to run
 # git remote add upstream <LINK TO ORIGINAL GIT>
-for repo in cokepc infoscreen; do
-    (
-    echo "\033[1;31m*************** Now pulling upstream ${repo} ***************\033[0m"
+for repo in $upstream; do
+    (echo "\033[1;31m*************** Now pulling upstream ${repo} ***************\033[0m"
     cd "${repo}" && git pull upstream master)
 done
 
