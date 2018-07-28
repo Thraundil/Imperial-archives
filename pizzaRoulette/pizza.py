16e075586204d30c1ec3fd7fe07b36e6ae921eec
thraundil <emilsbak@gmail.com>
2018-07-28 11:50:58 +0200

    updated for better randomness

--- pizzaRoulette/pizza.py
+++ pizzaRoulette/pizza.py
@@ -3,8 +3,10 @@
 # Af Emil S. Bak @ EGMONT 2018
 # Try it: 'python pizza.py <fil.txt>'
 #   <fil.txt> must contain <NAME> <NUMBER> only!
+#   <fil.txt> Numbers should be between 10-99
 
 import sys, getopt
+import random
 
 pizzaTal   = 42
 toppingTal = 12
@@ -17,10 +19,20 @@ def main(argv):
         file = open(str(argv[0]), "r")
 
         print "    **** PIZZAROULETTE ****  "
+        counter = 1
+
         for line in file:
             a,b = line.split()
-            print (a.ljust(8)+": Pizza #"+str(int(b)%pizzaTal).rjust(2)+", topping #" + str(int(b)%toppingTal))
+            print (a.ljust(8)+": Pizza #"+str(pizza(b,counter)).rjust(2)+", topping #" + str(topping(b,counter)))
+            counter += 1
 
+def pizza(userNumber, counter):
+    random.seed(int(userNumber)*counter)
+    return (random.randint(1, pizzaTal))
+
+def topping(userNumber, counter):
+    random.seed(int(userNumber)*counter)
+    return (random.randint(1, toppingTal))
 
 if __name__ == '__main__':
     main(sys.argv[1:])
